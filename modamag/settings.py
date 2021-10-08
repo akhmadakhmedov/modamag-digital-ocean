94dd0f17cfc7bb1d623fed81ad5da4953a6c96a7
akhmadakhmedov <ahmedov.thy@gmail.com>
2021-10-08 11:21:48 +0600

    env settings

--- modamag/settings.py
+++ modamag/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-cw)k4=wh74vk8!r30b_o0om3!kq$+8l-bio0_$yg923n7oicy8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool) #True
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
